94b8996ed6e48a93393e7096e0551bf452759a35
muggle <yuanzening@hlram.cn>
2019-01-27 11:29:53 +0800

    文件上传

--- poseidon-common/src/main/java/com/muggle/poseidon/utils/query/QueryManager.java
+++ poseidon-common/src/main/java/com/muggle/poseidon/utils/query/QueryManager.java
@@ -1,5 +1,12 @@
 package com.muggle.poseidon.utils.query;
 
+import lombok.Data;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
@@ -8,12 +15,25 @@ import java.util.List;
  * @author: muggle
  * @create: 2019-01-27 10:38
  **/
-
-public class QueryManager {
+@Data
+public class QueryManager<T> {
     private List<Query> queries;
     private boolean distinct;
     /** 数据分页开始行 */
     private Integer startRow;
     /** 每次取多少行数据 */
     private Integer rowSize;
+
+    public Specification<T> getSpec(){
+        return new Specification<T>() {
+            @Override
+            public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+                Predicate predicate = builder.isNull(root.get("deleteTime"));
+                queries.forEach(item->{
+                    if (item.setQueryType();)
+                });
+                return null;
+            }
+        };
+    }
 }
