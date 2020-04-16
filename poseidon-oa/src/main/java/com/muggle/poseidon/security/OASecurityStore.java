5196630c73ce32bb1187627d614f2ddad0b64eed
muggle <isocket@outlook.com>
2020-04-16 22:00:26 +0800

    webflux 搭建项目

---
+++ poseidon-oa/src/main/java/com/muggle/poseidon/security/OASecurityStore.java
@@ -0,0 +1,27 @@
+package com.muggle.poseidon.security;
+
+import com.muggle.poseidon.base.exception.BasePoseidonCheckException;
+import com.muggle.poseidon.store.SecurityStore;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/4/16
+ **/
+public class OASecurityStore implements SecurityStore {
+    @Override
+    public UserDetails getUserdetail(String s) throws BasePoseidonCheckException {
+        return null;
+    }
+
+    @Override
+    public String signUserMessage(UserDetails userDetails) {
+        return null;
+    }
+
+    @Override
+    public Boolean cleanToken(String s) {
+        return null;
+    }
+}
