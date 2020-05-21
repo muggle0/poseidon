aa9d73039c9fbb72f1cd43d0118c4ba7937a8845
muggle <isocket@outlook.com>
2020-05-21 21:02:57 +0800

    webflux 配置mybatis-plus

--- poseidon-oa/src/main/java/com/muggle/poseidon/controller/UserController.java
+++ poseidon-oa/src/main/java/com/muggle/poseidon/controller/UserController.java
@@ -2,7 +2,10 @@ package com.muggle.poseidon.controller;
 
 import com.muggle.code.SimpleCodeTemplate;
 import com.muggle.code.TableMessage;
+import com.muggle.poseidon.base.ResultBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -13,16 +16,21 @@ import java.util.Arrays;
  * @Date: 2020/5/21
  **/
 @RestController
+@RequestMapping("/")
 public class UserController {
 //    @Autowired
 
-    public static void main(String[] args) {
+    @GetMapping("/test")
+    public ResultBean<String> test(){
+        return ResultBean.successData("hello world");
+    }
 
+
+    public static void main(String[] args) {
         TableMessage tableMessage = new TableMessage();
         tableMessage.setUsername("root");
         tableMessage.setSwagger(true);
         tableMessage.setTableName(Arrays.asList("menu"));
-
         tableMessage.setAuthor("muggle");
         tableMessage.setBasePack("com.muggle");
         tableMessage.setDriver("com.mysql.jdbc.Driver");
