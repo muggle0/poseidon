fdfb16823ef0d0bba69358f4a305573b070bb8e8
muggle <isocket@outlook.com>
2020-05-24 18:01:26 +0800

    开启oa 项目

---
+++ poseidon-oa/src/main/java/com/muggle/oa/service/impl/OaTokenService.java
@@ -0,0 +1,47 @@
+package com.muggle.oa.service.impl;
+
+import com.muggle.poseidon.base.exception.SimplePoseidonCheckException;
+import com.muggle.poseidon.entity.AuthUrlPathDO;
+import com.muggle.poseidon.service.TokenService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/5/24
+ **/
+@Service
+public class OaTokenService implements TokenService {
+    @Override
+    public UserDetails getUserById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public boolean rooleMatch(Collection<? extends GrantedAuthority> collection, String s) {
+        return false;
+    }
+
+    @Override
+    public void saveUrlInfo(List<AuthUrlPathDO> list) {
+
+    }
+
+    @Override
+    public UserDetails login(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws SimplePoseidonCheckException {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
