7ac2001b26b76e6971b3eee0d2c50e5209e0532e
muggle <isocket@outlook.com>
2020-05-26 11:28:02 +0800

    lay-ui集成

---
+++ poseidon-oa/src/main/java/com/muggle/poseidon/oa/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.muggle.poseidon.oa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Description:
+ * @Author: muggle
+ * @Date: 2020/5/26
+ **/
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
