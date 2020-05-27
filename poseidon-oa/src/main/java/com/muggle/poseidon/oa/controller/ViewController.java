61426093b40eb88d9a08266679dd4a29dd3b6592
muggle <isocket@outlook.com>
2020-05-27 18:29:38 +0800

    lay-ui 框架集成

--- poseidon-oa/src/main/java/com/muggle/poseidon/oa/controller/ViewController.java
+++ poseidon-oa/src/main/java/com/muggle/poseidon/oa/controller/ViewController.java
@@ -1,7 +1,15 @@
 package com.muggle.poseidon.oa.controller;
 
+import com.muggle.poseidon.base.exception.BasePoseidonCheckException;
+import com.muggle.poseidon.util.UserInfoUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Description:
@@ -10,12 +18,22 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 @Controller
 public class ViewController {
+    private static final Logger log = LoggerFactory.getLogger(ViewController.class);
 
-  /*  @GetMapping("/views")
-    public String index(){
-        return "/index.html";
-    }*/
+    @GetMapping("/")
+    public String index(HttpServletResponse response){
+        try {
+            if (UserInfoUtils.getUserInfo()!=null){
+                response.sendRedirect("http://localhost:8080/views/index.html");
+            }
+        } catch (BasePoseidonCheckException e) {
+           log.error("获取用户信息异常",e);
+        } catch (IOException e) {
+            return "template/tips/error.html";
+        }
+        return "user/login.html";
+    }
 
 
-
+/**/
 }
