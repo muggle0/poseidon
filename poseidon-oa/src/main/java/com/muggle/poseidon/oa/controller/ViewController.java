85bc27a1b8b6e8b64855fe75a6d0f32175494b21
muggle <isocket@outlook.com>
2020-05-27 16:48:54 +0800

    lay-ui 框架集成

--- poseidon-oa/src/main/java/com/muggle/poseidon/oa/controller/ViewController.java
+++ poseidon-oa/src/main/java/com/muggle/poseidon/oa/controller/ViewController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
  **/
 @Controller
 public class ViewController {
+
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "view/index";
     }
+
+
+
 }
