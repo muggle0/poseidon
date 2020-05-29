2e2270b519616c49d906db198913819e60719c52
muggle <isocket@outlook.com>
2020-05-29 14:25:00 +0800

    静态文件

---
+++ poseidon-common/src/main/java/com/muggle/poseidon/service/oa/IOaUrlInfoService.java
@@ -0,0 +1,16 @@
+package com.muggle.poseidon.service.oa;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.muggle.poseidon.entity.oa.OaUrlInfo;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author muggle
+ * @since 2020-05-29
+ */
+public interface IOaUrlInfoService extends IService<OaUrlInfo> {
+
+}
