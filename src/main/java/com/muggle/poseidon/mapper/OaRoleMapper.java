30eda218f9f5a49ec0a5716cd5f93c122cdb85e6
muggle <isocket@outlook.com>
2020-11-12 17:17:46 +0800

    todo

---
+++ src/main/java/com/muggle/poseidon/mapper/OaRoleMapper.java
@@ -0,0 +1,24 @@
+package com.muggle.poseidon.mapper;
+
+import java.util.List;
+
+import com.muggle.poseidon.entity.OaRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author muggle
+ * @since 2020-11-12
+ */
+public interface OaRoleMapper extends BaseMapper<OaRole> {
+
+    /**
+     * 根据用户id查询权限
+     * * @param id
+     * @return
+     */
+    List<OaRole> selectByUserId(Long id);
+}
--- src/main/java/com/muggle/poseidon/service/helper/impl/NormalLoginHelper.java
+++ src/main/java/com/muggle/poseidon/service/helper/impl/NormalLoginHelper.java
@@ -1,11 +1,18 @@
 package com.muggle.poseidon.service.helper.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.muggle.poseidon.base.exception.SimplePoseidonCheckException;
+import com.muggle.poseidon.entity.OaRole;
 import com.muggle.poseidon.entity.OaUserInfo;
+import com.muggle.poseidon.mapper.OaRoleMapper;
 import com.muggle.poseidon.service.helper.LoginHelper;
 import com.muggle.poseidon.service.manager.UserInfoManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,9 +24,24 @@ import org.springframework.stereotype.Service;
 public class NormalLoginHelper implements LoginHelper {
     @Autowired
     UserInfoManager userInfoManager;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private OaRoleMapper oaRoleMapper;
     @Override
     public UserDetails login(String username, String password) throws SimplePoseidonCheckException {
         OaUserInfo userInfo= ((OaUserInfo) userInfoManager.loadUserByUsername(username));
-        return null;
+        boolean matches = passwordEncoder.matches(password, userInfo.getPassword());
+        if (!matches){
+            throw new SimplePoseidonCheckException("密码错误");
+        }
+        List<OaRole>roles= oaRoleMapper.selectByUserId(userInfo.getId());
+        List<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        roles.forEach(role->{
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getRoleCode());
+            authorities.add(authority);
+        });
+        userInfo.setAuthorities(authorities);
+        return userInfo;
     }
 }
