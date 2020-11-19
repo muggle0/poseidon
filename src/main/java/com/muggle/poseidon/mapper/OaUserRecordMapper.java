af387612bcefc3c5e1f387d8e732ceb2b21d49d7
muggle0 <isocket@outlook.com>
2020-11-19 16:12:47 +0800

    注册

---
+++ src/main/java/com/muggle/poseidon/mapper/OaUserRecordMapper.java
@@ -0,0 +1,16 @@
+package com.muggle.poseidon.mapper;
+
+import com.muggle.poseidon.entity.pojo.OaUserRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户日志 Mapper 接口
+ * </p>
+ *
+ * @author muggle
+ * @since 2020-11-17
+ */
+public interface OaUserRecordMapper extends BaseMapper<OaUserRecord> {
+
+}
